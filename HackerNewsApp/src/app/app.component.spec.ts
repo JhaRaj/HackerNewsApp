26584681cd4cb29b0bd28700b2f6d64bf42f9ea1
JhaRaj <30748353+JhaRaj@users.noreply.github.com>
2024-08-06 00:08:22 +0530

    added test cases for code coverage in app.component.spec.ts

--- HackerNewsApp/src/app/app.component.spec.ts
+++ HackerNewsApp/src/app/app.component.spec.ts
@@ -1,50 +1,79 @@
-import {ComponentFixture,TestBed,fakeAsync} from '@angular/core/testing';
-import {AppComponent} from './app.component';
+import {ComponentFixture,TestBed,waitForAsync} from '@angular/core/testing';
+import {AppComponent,Story} from './app.component';
 import {provideHttpClient} from '@angular/common/http';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {provideHttpClientTesting} from "@angular/common/http/testing";
 import {HNewsService} from './hnews.service';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { of } from 'rxjs';
 
 describe('AppComponent', () => {
-
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
-  let service: HNewsService;
- 
-  beforeEach(async() => {
+  const result:Story[]= [{ title: 'story', url: 'https://api/story' }];
+
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [NoopAnimationsModule,AppComponent],
-      providers: [
-                provideHttpClient(),
-                provideHttpClientTesting(),
-                HNewsService,
-              ],
+      imports: [NoopAnimationsModule,MatPaginatorModule],
+      providers: [HNewsService,provideHttpClient(),provideHttpClientTesting(),AppComponent],
     }).compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
-    service = TestBed.inject(HNewsService);
-    fixture.detectChanges();
   });
 
-  it('should create the component', fakeAsync(() => {
+  it('should create app component', () => {
     expect(component).toBeTruthy();
-  }));
+  });
 
-  it('should populate the dataSource', fakeAsync(() => {
-    component.dataSource=new MatTableDataSource([{title:'google',url:'https://www.google.com'}]);
-    fixture.detectChanges();
-    component.ngOnInit();
-    expect(component.dataSource.data).toEqual([{title:'google',url:'https://www.google.com'}]);
-  }));
+  it('should load stories to table dataSource on method call getStoryData',()=>{
+    let service=fixture.debugElement.injector.get(HNewsService);
+    spyOn(service,'getData').and.callFake(()=>{
+      return of(result);
+    })
+    component.getStoryData();
+    expect(component.dataSource.data).toEqual(result);
+  });
 
-  it('should filter the dataSource', fakeAsync(() => {
-    component.dataSource.filter = 'title';
+  it('should filter data from table dataSource',()=>{
+    let service=fixture.debugElement.injector.get(HNewsService);
+    spyOn(service,'getData').and.callFake(()=>{
+      return of(result);
+    });
+    component.getStoryData();
+    component.applyFilter('story')
+    expect(component.dataSource.filteredData[0].title).toContain('story');
+  });
+
+  it('should handle pagination', () => {
+    let service=fixture.debugElement.injector.get(HNewsService);
+    spyOn(service,'getData').and.callFake(()=>{
+      return of(result);
+    });
+
+    component.getStoryData();
     fixture.detectChanges();
-    component.ngOnInit();
-    expect(component.dataSource.filter).toBe('title');
-  }));
+
+    const mockPaginator: MatPaginator = {
+      length: 200,
+      pageSize: 5,
+      pageIndex: 0,
+      pageSizeOptions: [5, 10, 25, 100],
+      hasPreviousPage: () => component.paginator.pageIndex > 0,
+      hasNextPage: () =>component.paginator.pageIndex < 40,
+    } as MatPaginator;
+    component.paginator = mockPaginator;
+    component.paginator.pageIndex = 1;
+    while (component.paginator.hasNextPage()) 
+    {
+      component.paginator.pageIndex++;  
+    };
+    expect(component.paginator.pageIndex).toEqual(40);
+  }); 
+
 });
+
+
+
